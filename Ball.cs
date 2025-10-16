7866ec5f3437e6ccaa51649d1e98428b1bbe8a4a
Woody0810 <134690334+Woody0810@users.noreply.github.com>
2025-10-16 14:38:07 +0200

    setup

---
+++ Ball.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_ConsolePong
+{
+    internal class Ball
+    {
+        int x;
+        int y;
+        int xvelocity;
+        int yvelocity;
+        public Ball(int x, int y, int xvelocity, int yvelocity)
+        {
+            this.x = x;
+            this.y = y;
+            this.xvelocity = xvelocity;
+            this.yvelocity = yvelocity;
+        }
+        public void MoveBall()
+        {
+
+        }
+        public void DrawBall()
+        {
+        }
+        public void CheckCollisions(Paddel p1, Paddel p2, int width, int height)
+        {
+
+        }
+    }
+
+}
---
+++ Paddel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_ConsolePong
+{
+    internal class Paddel
+    {
+        int x;
+        int y;
+        int size;
+
+        public Paddel(int x, int y, int size)
+        {
+            this.x = x;
+            this.y = y;
+            this.size = size;
+        }
+        public void MovePaddel(int yAmount)
+        {
+
+        }
+        public void DrawPaddel()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("*");
+        }
+        
+    }
+}
